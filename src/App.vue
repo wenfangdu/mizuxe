df60bb64e4cae713cee72017d42ec5c126d94a42
Leon <849995097@qq.com>
2017-09-10 19:56:06 +1200

    completed newsletter xs

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-sm navbar-light py-4">
-      <div class="container">
-        <a href="index.html" class="navbar-brand">
-          <img src="./assets/img/mlogo.png" width="50" height="50" alt="mlogo">
-          <h3 class="d-inline align-middle">BS2</h3>
-        </a>
-        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a href="#about" class="nav-link">About</a>
-            </li>
-            <li class="nav-item">
-              <a href="#authors" class="nav-link">Authors</a>
-            </li>
-            <li class="nav-item">
-              <a href="#contact" class="nav-link">Contact</a>
-            </li>
-          </ul>
+    <header>
+      <nav class="navbar navbar-expand-sm navbar-light py-4">
+        <div class="container">
+          <a href="index.html" class="navbar-brand">
+            <img src="./assets/img/mlogo.png" width="50" height="50" alt="mlogo">
+            <h3 class="d-inline align-middle">BS2</h3>
+          </a>
+          <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a href="#about" class="nav-link">About</a>
+              </li>
+              <li class="nav-item">
+                <a href="#authors" class="nav-link">Authors</a>
+              </li>
+              <li class="nav-item">
+                <a href="#contact" class="nav-link">Contact</a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
     <!-- showcase -->
     <section id="showcase">
       <div class="overlay overlay--primary text-white">
@@ -36,6 +38,15 @@
             <i class="fa fa-arrow-right"></i>Read More
           </a>
         </div>
+      </div>
+    </section>
+    <section id="newsletter" class="bg-dark text-white">
+      <div class="container py-5">
+        <form action="post">
+          <input type="text" class="form-control form-control-lg mb-2" placeholder="Enter Name">
+          <input type="text" class="form-control form-control-lg mb-2" placeholder="Enter Email">
+          <button class="btn btn-primary btn-lg btn-block"><i class="fa fa-envelope-open-o"></i>Subscribe</button>
+        </form>
       </div>
     </section>
   </div>
