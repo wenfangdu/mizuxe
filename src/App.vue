6798d92efeee375ac5ff7136fa59f5d9935fe7d7
Leon <849995097@qq.com>
2017-09-11 10:27:56 +1200

    completed a few sections xs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- main header -->
     <header>
       <nav class="navbar navbar-expand-sm navbar-light py-4">
         <div class="container">
@@ -13,21 +14,21 @@
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a href="#about" class="nav-link">About</a>
+                <a href="#about-section" class="nav-link">About</a>
               </li>
               <li class="nav-item">
-                <a href="#authors" class="nav-link">Authors</a>
+                <a href="#authors-section" class="nav-link">Authors</a>
               </li>
               <li class="nav-item">
-                <a href="#contact" class="nav-link">Contact</a>
+                <a href="#contact-section" class="nav-link">Contact</a>
               </li>
             </ul>
           </div>
         </div>
       </nav>
     </header>
-    <!-- showcase -->
-    <section id="showcase">
+    <!-- showcase section-->
+    <section id="showcase-section">
       <div class="overlay overlay--primary text-white">
         <div class="container text-center">
           <h1 class="py-5 mt-5">
@@ -40,15 +41,61 @@
         </div>
       </div>
     </section>
-    <section id="newsletter" class="bg-dark text-white">
+    <!-- newsletter section -->
+    <section id="newsletter-section" class="bg-dark text-white">
       <div class="container py-5">
         <form action="post">
-          <input type="text" class="form-control form-control-lg mb-2" placeholder="Enter Name">
-          <input type="text" class="form-control form-control-lg mb-2" placeholder="Enter Email">
+          <div class="form-group">
+            <input type="text" class="form-control form-control-lg" placeholder="Enter Name">
+          </div>
+          <div class="form-group">
+            <input type="text" class="form-control form-control-lg" placeholder="Enter Email">
+          </div>
           <button class="btn btn-primary btn-lg btn-block"><i class="fa fa-envelope-open-o"></i>Subscribe</button>
         </form>
       </div>
     </section>
+    <!-- boxes section -->
+    <section id="boxes-section">
+      <div class="container py-5">
+        <div class="card text-center border-primary">
+          <div class="card-body">
+            <h3 class="text-primary">Be Better</h3>
+            <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, corrupti?</p>
+          </div>
+        </div>
+        <div class="card text-center bg-primary text-white">
+          <div class="card-body">
+            <h3>Be Smarter</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, corrupti?</p>
+          </div>
+        </div>
+        <div class="card text-center border-primary">
+          <div class="card-body">
+            <h3 class="text-primary">Be Stronger</h3>
+            <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, corrupti?</p>
+          </div>
+        </div>
+        <div class="card text-center bg-primary text-white">
+          <div class="card-body">
+            <h3>Be Faster</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, corrupti?</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- about section -->
+    <section id="about" class="text-center bg-light">
+      <div class="container py-5">
+        <div class="info-header mb-5">
+          <h1 class="text-primary pb-3">
+            Why this book?
+          </h1>
+          <p class="lead pb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit dolores quia amet numquam architecto dolore!</p>
+        </div>
+      </div>
+    </section>
+
   </div>
 </template>
 
